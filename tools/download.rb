3ac1d868d9480a3896882b95b0bf2d9f92859084
Sho Hashimoto <hashimoto@shokai.org>
2012-02-27 23:16:35 +0900

    modify disconnecting of slow download

--- tools/download.rb
+++ tools/download.rb
@@ -39,6 +39,7 @@ loop do
     fname = Digest::MD5.hexdigest(video.video_url)
     begin
       stream_count = 0
+      start_at = Time.now.to_i
       open("#{@@dir}/#{fname}", 'w+'){|out|
         $VERBOSE = nil
         Curl::Easy.perform(video.video_url){|easy|
@@ -50,18 +51,15 @@ loop do
           end
           easy.on_body do |data|
             out.print data
-            stream_count += 1
-            if stream_count % 1000 == 0
-              puts "#{easy.download_speed/1000}kbps"
-              if easy.download_speed/1000 < params[:min_speed].to_i
-                raise DownloadError.new("download too slow (#{easy.download_speed/1000}kbps)")
-              end
+            puts "#{easy.download_speed/1000}kbps" if (stream_count += 1) % 1000 == 0
+            if Time.now.to_i-20 > start_at and easy.download_speed/1000 < params[:min_speed].to_i
+              raise DownloadError.new("download too slow (#{easy.download_speed/1000}kbps), #{stream_count} blocks received.")
             end
           end
         }
       }
       exif = MiniExiftool.new "#{@@dir}/#{fname}"
-      raise "#{exif['mime_type']} is not video file" unless exif['mime_type'] =~ /^video\/.+/i
+      raise "#{exif['mime_type']} is not video" unless exif['mime_type'] =~ /^video\/.+/i
       video[:file] = fname
       video[:download_at] = Time.now.to_i
       video.save
