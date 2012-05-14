8e438943bf5651eec7d3bd61b79998b3df140150
Sho Hashimoto <hashimoto@shokai.org>
2012-05-14 09:30:35 +0900

    tools/delete_old_video

--- tools/delete_old_video.rb
+++ tools/delete_old_video.rb
@@ -1,14 +1,14 @@
 #!/usr/bin/env ruby
 require File.dirname(__FILE__)+'/init'
 
-videos = Video.where(:file => /.+/, :video_url => /^http.+/)
+videos = Video.where(:file => /.+/, :video_url => /^http.+/).asc(:_id)
 
 puts "#{videos.count} videos in queue"
 
 videos.each{|v|
   puts "#{v.title} - #{v.url}"
-  puts "  delete? [y/N]"
-  if gets =~ /y/i
+  puts "  delete? [Y/n]"
+  if gets !~ /n/i
     begin
       fname = "#{@@dir}/#{v.file}"
       v.delete_file!
