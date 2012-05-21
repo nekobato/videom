ca76a412e3745a04e60ce2f39fb1cdcf4da67c8a
Sho Hashimoto <hashimoto@shokai.org>
2012-05-21 20:44:02 +0900

    move next/prev_page_url to helper

--- helpers/helper.rb
+++ helpers/helper.rb
@@ -2,3 +2,23 @@
 def app_root
   "#{env['rack.url_scheme']}://#{env['HTTP_HOST']}#{env['SCRIPT_NAME']}"
 end
+
+def next_page_url
+  if @tag
+    return "#{app_root}/tag/#{@tag}?page=#{@page+1}&per_page=#{@per_page}"
+  elsif @word
+    return "#{app_root}/search/#{@word}?page=#{@page+1}&per_page=#{@per_page}"
+  else
+    return "#{app_root}/?page=#{@page+1}&per_page=#{@per_page}"
+  end
+end
+
+def prev_page_url
+  if @tag
+    return "#{app_root}/tag/#{@tag}?page=#{@page-1}&per_page=#{@per_page}"
+  elsif @word
+    return "#{app_root}/search/#{@word}?page=#{@page-1}&per_page=#{@per_page}"
+  else
+    return "#{app_root}/?page=#{@page-1}&per_page=#{@per_page}"
+  end
+end
